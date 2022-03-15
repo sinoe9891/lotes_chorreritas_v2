6b6b55c164bbb9a8f1c980ad176b7b13b67406df
sinoe9891 <sinoeproducciones@gmail.com>
2022-03-14 23:10:12 -0600

    cambios gitignore

--- includes/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$conn = new mysqli("localhost","root","","lotes_chorreritas");
-	// $conn = new mysqli("151.106.100.46","creat312_sinoe","Stark989121","creat312_lotes");
-	 
-	if($conn->connect_error){
-		echo 'Conexión Fallida: ', $conn->connect_error;
-	}
-	$conn->set_charset("utf8");	
-?>
