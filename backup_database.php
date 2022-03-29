acb9356babaa53379dc8461ac3dafd1c15560fa9
sinoe9891 <sinoeproducciones@gmail.com>
2022-03-29 01:09:36 -0600

    enlace a descargar backup

--- backup_database.php
+++ backup_database.php
@@ -2,11 +2,12 @@
 <?php
 // exportarTablas("localhost", "root", "", "lotes_chorreritas");
 // backupDatabaseTables("151.106.100.46","creat312_sinoe","Stark989121","creat312_lotes");
+ob_start();
 $pass = $_GET['pass'];
 if ($pass == "Stark9891") {
 	exportarTablas();
 }else{
-	header ('Location: index.php');
+	// header ('Location: index.php');
 }
 function exportarTablas()
 {
@@ -52,22 +53,24 @@ function exportarTablas()
 		}
 		$backupSQL .= "\n";
 	}
-
+	
 	if (!empty($backupSQL)) {
 		$backup_file_name = 'backup_db_'. $database_name .'_'. date('d-m-Y') .'_'. time() . '.sql';
+		header ('location: respaldos/'.$backup_file_name);
 		$fileHandler = fopen('respaldos/'.$backup_file_name, 'w+');
 		$number_of_lines = fwrite($fileHandler, $backupSQL);
 		fclose($fileHandler);
 
-		header('Content-Description: File Transfer');
-		header('Content-Type: application/octet-stream');
-		header('Content-Disposition: attachment; filename=' . basename($backup_file_name));
-		header('Content-Transfer-Encoding: binary');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header('Content-Length: ' . filesize($backup_file_name));
+		// header('Content-Description: File Transfer');
+		// header('Content-Type: application/octet-stream');
+		// header('Content-Disposition: attachment; filename=' . basename($backup_file_name));
+		// header('Content-Transfer-Encoding: binary');
+		// header('Expires: 0');
+		// header('Cache-Control: must-revalidate');
+		// header('Pragma: public');
+		// header('Content-Length: ' . filesize($backup_file_name));
 		ob_clean();
 		flush();
 	}
+	ob_start();
 }
