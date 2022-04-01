98012a3bfa9bce0092591dc7d097f1616050522a
sinoe9891 <sinoeproducciones@gmail.com>
2022-03-31 19:52:17 -0600

    cambios enlaces

--- index.php
+++ index.php
@@ -25,26 +25,28 @@ include 'includes/templates/sidebar.php';
 					<div class="col-6 col-lg-3 col-md-6">
 						<div class="card">
 							<div class="card-body px-3 py-4-5">
-								<div class="row">
-									<div class="col-md-4">
-										<div class="stats-icon purple">
-											<i class="iconly-boldShow"></i>
+								<a href="clientes.php">
+									<div class="row">
+										<div class="col-md-4">
+											<div class="stats-icon purple">
+												<i class="iconly-boldShow"></i>
+											</div>
+										</div>
+										<div class="col-md-8">
+											<h6 class="text-muted font-semibold">Clientes</h6>
+											<?php
+											$solicitudes = obtenerNumeroClientes('id');
+											if ($solicitudes->num_rows > 0) {
+												echo '<h6 class="font-extrabold mb-0">' . $solicitudes->num_rows . '</h6>';
+											} else {
+												// no hay tareas
+												echo '<h3>Registros<br>de Clientes</h3>';
+											}
+											?>
+											<!-- <h6 class="font-extrabold mb-0">112.000</h6> -->
 										</div>
 									</div>
-									<div class="col-md-8">
-										<h6 class="text-muted font-semibold">Clientes</h6>
-										<?php
-										$solicitudes = obtenerNumeroClientes('id');
-										if ($solicitudes->num_rows > 0) {
-											echo '<h6 class="font-extrabold mb-0">' . $solicitudes->num_rows . '</h6>';
-										} else {
-											// no hay tareas
-											echo '<h3>Registros<br>de Clientes</h3>';
-										}
-										?>
-										<!-- <h6 class="font-extrabold mb-0">112.000</h6> -->
-									</div>
-								</div>
+								</a>
 							</div>
 						</div>
 					</div>
